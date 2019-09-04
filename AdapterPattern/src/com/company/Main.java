4bd211cb8257a8c164bf5825d669eafcb4e53a57
madnan <adnan.merusic@hycu.com>
2019-09-04 20:30:00 +0200

    Adapter Pattern

---
+++ AdapterPattern/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        IFighter warrior = new Fighter();
+        warrior.attack();
+        //Wizzard is IWizzard type here..this doesnt suite us, need addapter to make it IFighter type
+        IWizzard merlini = new Wizzard();
+        merlini.castSpell();
+
+        IFighter merliniFighter = new WizzardToFighterAdapter(merlini);
+
+        merliniFighter.attack();
+    }
+}
